07798a91c072143631878e8842d6525dc59adce2
T123S <98583520+T123S@users.noreply.github.com>
2026-01-20 23:42:12 +0100

    Add files via upload
    Optimierungen

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'krake-v6';
+const CACHE_NAME = 'krake-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -31,5 +31,4 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request)
       .then((response) => response || fetch(event.request))
   );
-
 });
